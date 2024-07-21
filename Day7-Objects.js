77df1b91309c607a179bb8a33866d6592c45d2e1
Muhammad Sami <muhammadsami1242@gmail.com>
2024-07-21 23:52:27 +0500

    day 8 half completed

--- Day7-Objects.js
+++ Day7-Objects.js
@@ -41,4 +41,73 @@ console.log(object);
 
 // Task 5: Create a nested object representing a library with properties like name and books (an array of book objects), and log the library object to the console.
 
+const library = {
+  name: "Ali",
+  books: [
+    {
+      book: "Rich Dad Poor Dad",
+      year: 2023,
+      method3: function () {
+        return this.book + " " + this.year;
+      },
+    },
+    {
+      book: "Javascript",
+      year: 2018,
+      method3: function () {
+        return this.book + " " + this.year;
+      },
+    },
+    {
+      book: "C++",
+      year: 2001,
+      method3: function () {
+        return this.book + " " + this.year;
+      },
+    },
+  ],
+};
+console.log(library);
+
 // Task 6: Access and log the name of the library and the titles of all books in the library.
+
+console.log(library.name);
+library.books.map((item) => {
+  console.log(item);
+});
+
+// Activity 4: The this keyword
+
+// Task 7: Add a method to the book object that uses the this keyword to return a string with the book title and year, and log the result of calling this method.
+
+const booksdetails = library.books.map((item) => {
+  return item.method3();
+});
+console.log(booksdetails);
+// console.log(library.books[2].year);
+
+// Activity 5: Object iteration
+// Task 8: Use a for in loop to iterate over the properites of the book object and log each property and it value.
+
+library.books.forEach((book, index) => {
+  console.log(`Book ${index + 1}:`);
+  for (let property in book) {
+    if (book.hasOwnProperty(property)) {
+      console.log(`${property}: ${book[property]}`);
+    }
+  }
+  console.log("");
+});
+
+// Task 9 : Use object Keys and Object values methods to log all the keys and values of the book object.
+library.books.forEach((book, index) => {
+  console.log(`Book ${index + 1}:`);
+
+  const keys = Object.keys(book);
+  console.log("Keys:", keys);
+
+  const values = Object.values(book);
+  console.log("Values:", values);
+
+  console.log("");
+});
---
+++ Day8-ES6features.js
@@ -0,0 +1,63 @@
+// Day 8: ES6 Features
+
+// Task / Activities :
+
+// Activity 1: Template literals
+
+// Task 1 : Use Template literals to create a string that inculdes varible , for a person name and age, and log the string to the console.
+
+const name = "Sami";
+const age = 23;
+console.log(`Name is ${name} and age is ${age}`);
+
+// Task 2 : Create a multi-line string using template literals and log it to the console.
+console.log(
+  `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio, est. Minima aperiam cupiditate aspernatur deserunt mollitia exercitationem non similique hic ipsa ut laudantium quisquam iste nostrum rerum, magnam architecto saepe.`
+);
+
+// Activity 2: Destructuring
+// Task 3: Use array Destructuring to extract the first and second elements from array of numbers and log them to the console.
+
+const array = ["leo", "honey", "book"];
+
+const [first, second] = array;
+console.log(first, second);
+
+// Task 4: Use object Destructuring to extract the title and author from a book object and log them to the console.
+
+const obj = {
+  title: "atomic habits",
+  author: "Muhammad Sami",
+};
+
+const { title, author } = obj;
+console.log(`${title} ${author}`);
+
+// Activity 3: Spread and Rest Operators
+
+// Task 5: Use the spread operator to create a new array that includes all elements of an existing array plus additonal elements, and log the new array to the console.
+
+const oldArray = [1, 2, 4, 5, 6];
+const newArray = [...oldArray, 7, 8, 9];
+console.log(newArray);
+
+// Task 6: Use the rest operator in a function to accept an arbitrary number of arguments , sum them , and return the result.
+
+const rest = (...values) => {
+  return values.reduce((first, second) => first + second);
+};
+console.log(rest(2, 5));
+
+// Activity 4: Default parameters
+// Task 7: Write a function that takes two parameters and return their products, with the second parameter having a default value of 1. log the result of calling this function with and without the second parameter.
+
+const defaultParameters = (number1, number2 = 1) => {
+  return number1 * number2;
+};
+console.log(defaultParameters(2, 2));
+console.log(defaultParameters(5));
+
+// Activity 5: Enhanced object literals
+// Task 8:
+
+// Task 9:
