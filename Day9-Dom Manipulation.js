79a3c2dbf398e2c006b699549c6c4294b5ef569f
muhammadsami <muhammadsami1242@gmail.com>
2024-07-22 23:59:10 +0500

    Day9 half completed

---
+++ Day9-Dom Manipulation.js
@@ -0,0 +1,37 @@
+// Day 9: Dom - Manipulation
+// Task / Activities:
+
+// Activity 1 : Selection and Manipulation Elememts
+
+// Task 1 : Select an HTML Element by its id and change its text content.
+
+let text = document.getElementById('text').textContent = 'Muhammad Sami';
+
+
+// Task 2 : Select an HTML Element by its class and change its background color.
+let changeColor = document.getElementsByClassName('changeColor');
+changeColor.style.background = 'red';
+
+// Activity 2: Creating and Appending Elements
+
+// Task 3: Create a new div element with some text content and appended to the body.
+
+// Task 4: Create a new li element and add it to the existing ul list.
+
+// Activity 3: Removing Elements
+
+// Task 5: Select an HTML element and remove it from the Dom.
+
+// Task 6: Remove the last child of a specfic HTML element.
+
+// Activity 4: Modifying Attributes and classes
+
+// Task 7: Select an HTML element and change one of its attributes.(src of an img tag)
+
+// Task 8: Add and remove css class to/from an HTML element.
+
+// Activity 5: Event Handling
+
+// Task 9 : Add a clcik listener to a button that changes the text content of a paragraph.
+
+// Task 10: Add a mouseover event listener to an element that changes its border color.
