4ce45af2c5a1054127c34a952cc181fc50810efa
muhammadsami <muhammadsami1242@gmail.com>
2024-07-23 23:51:17 +0500

    Day 9

--- Day9-Dom Manipulation.js
+++ Day9-Dom Manipulation.js
@@ -9,26 +9,44 @@ let text = document.getElementById('text').textContent = 'Muhammad Sami';
 
 
 // Task 2 : Select an HTML Element by its class and change its background color.
-let changeColor = document.getElementsByClassName('changeColor');
-changeColor.style.background = 'red';
-
+let change = document.getElementsByClassName('changeColor');
+for (i = 0; i < change.length; i++) {
+    change[i].style.background = "red";
+}
 // Activity 2: Creating and Appending Elements
 
 // Task 3: Create a new div element with some text content and appended to the body.
 
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "created a new div";
+const parent = document.querySelector("body");
+parent.appendChild(newDiv);
 // Task 4: Create a new li element and add it to the existing ul list.
+const ul = document.getElementById("ul");
+const li = document.createElement("li");
+li.textContent = "Monogo";
+ul.appendChild(li);
 
 // Activity 3: Removing Elements
-
 // Task 5: Select an HTML element and remove it from the Dom.
+
+const remove = document.getElementById("remove");
+remove.remove();
 
 // Task 6: Remove the last child of a specfic HTML element.
 
-// Activity 4: Modifying Attributes and classes
+const mylist = document.getElementById("mylist");
+mylist.removeChild(mylist.firstElementChild);
 
+// Activity 4: Modifying Attributes and classes
 // Task 7: Select an HTML element and change one of its attributes.(src of an img tag)
 
+const image = document.getElementById("image");
+image.setAttribute("src", "https://www.w3schools.com/w3images/lights.jpg");
+
 // Task 8: Add and remove css class to/from an HTML element.
+
+
 
 // Activity 5: Event Handling
 
