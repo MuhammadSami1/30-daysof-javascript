e4d5435100cfdb3130c5241e5bf3b59ce522a4f0
muhammadsami <muhammadsami1242@gmail.com>
2024-07-29 23:08:31 +0500

    day 13 half completed

---
+++ Day15-Closures.js
@@ -0,0 +1,26 @@
+// Day 15: Closures
+// Task / Activities:
+
+// Activity 1: Understanding Closures
+// Task 1: Write a function that returns another, where the inner function accesses a variable from the outer function scope. Call the inner function and log the result.
+
+
+// Task 2: Create a closure that maintains a private counter. Implement function to increment and get the current value of the counter.
+
+// Activity 2: Practical Closures
+// Task 3: Write a function that generates unique IDs. Use a closure to keep track of the last generated ID and increment it with each call.
+
+
+// Task 4: Create a closure that captures a users name and returns a function that greets the user by name.
+
+// Activity 3: Closures in Loops
+// Task 5: Write a loop that creates an array of functios. Each function should log its index when called. Use a closure to ensure each function logs the correct index.
+
+// Activity 4: Module Pattern
+// Task 6: Use closure to create a simple module for managing a collection of items. Implement methods to add, remove, and list items.
+
+
+// Activity 5 : Memoization
+// Task 7: Write a function that memoizes the result of another function. Use a closure to store the results of previous computations.
+
+// Task 8: Create a memoized version of a function that calculates the factorial of a Number. 
